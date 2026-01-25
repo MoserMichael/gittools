20e77741b68f2238bb5d9768d798a869cb4df686
MoserMichael <mosermichael@gmail.com>
2026-01-25 13:35:11 +0200

    add search_author.sh

--- search_author.sh
+++ search_author.sh
@@ -8,15 +8,19 @@ if [[ $gituser == "" ]]; then
     cat <<EOF
 usage: $o <github-user>
 
-shows number of commits of gituser, sorts the branch names by commit count, shows date of last commiit.
+shows number of commits of gituser in all remote branches, sorts the branch names by commit count, shows date of last commiit.
+
+Note: does not search the local branch
 EOF
     exit 1
 fi
 
+set -x
+
 out=""
-for branch in $(git branch -r); do
+for branch in $(git branch -r | awk '{print $1}'); do
     if [[ $branch != "origin/HEAD" ]]; then
-        cnt=$(git log ${branch} | grep -e '^Author:' | grep ${gituser} | wc -l | tr -d '[:space:]')
+        cnt=$(git log ${branch} | grep -e '^Author: '${gituser} | wc -l | tr -d '[:space:]')
         if [[ $cnt != "0" ]]; then
             last_commit_date=$(git log ${branch} | grep -A 1 -e '^Author: '${gituser} | sed -n '2p')
             printf -v out "%s\n" "branch ${branch} user: ${gituser} commit-count: ${cnt} last_commit_date: ${last_commit_date}"
